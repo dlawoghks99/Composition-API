e1ed2bda28dc6c79e590ee26f6f1e996982f1832
JeaHwanLim <dlawoghks5708@gmail.com>
2024-02-29 14:52:37 +0900

    vue3 compositionAPI 기본문법

--- vue-composition/src/App.vue
+++ vue-composition/src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">변경</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    // data() {
+    //     return {
+    //         message: 'heelo'
+    //     }
+    // },
+    setup(){
+        const message = ref('hello');
+
+        function changeMessage(){
+            message.value = 'hi';
+        }
+
+        return {message, changeMessage}
+    },
+    // methods:{
+    //     changeMessage(){
+    //         this.message = 'hi'
+    //     }
+    // }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
